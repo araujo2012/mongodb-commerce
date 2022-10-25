5cdc0e5afa824bcea903cf862d45a3ead7c45d2f
araujo2012 <araujo2012@hotmail.com>
2022-10-24 21:35:40 -0300

    fix: change tag value to contem sodio

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } },
-  { $push: { tags: "muito sódio" } },
+  { $push: { tags: "contém sódio" } },
 );
 db.produtos.find(
   {},
